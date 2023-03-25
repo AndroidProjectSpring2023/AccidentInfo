c0e95499cff721d8d02bef702f63bc7dbe2e2942
S555811 <112186550+S555811@users.noreply.github.com>
2023-03-25 18:12:42 -0500

    Added Profile Activity Business Logic.

--- app/src/main/java/com/example/sundari/accidentinfo/ProfileActivity.java
+++ app/src/main/java/com/example/sundari/accidentinfo/ProfileActivity.java
@@ -1,20 +1,32 @@
 package com.example.sundari.accidentinfo;
 
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView profileName, profileEmpID, profileEmail, profilePhone, profileOccupation, profileRegion;
     private Button  ChangePwd;
-
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private ProgressDialog progressDialog;
+    private DatabaseReference databaseReference;
     String Uid;
 
     @Override
@@ -32,7 +44,39 @@ public class ProfileActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(this);
 
+        progressDialog.setMessage("Loading!");
+        progressDialog.show();
+
+        Uid = firebaseAuth.getUid();
+        databaseReference = firebaseDatabase.getReference("User").child(Uid);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                progressDialog.dismiss();
+
+                profileName.setText("Name   : " + userProfile.getUserName());
+                profileEmpID.setText("EmpID  : " + userProfile.getUserEmpID());
+                profileEmail.setText("Email    : " + userProfile.getUserEmail());
+                profilePhone.setText("Phone No   : " + userProfile.getUserPhone());
+                profileOccupation.setText("Occupation: " + userProfile.getUserOccupation());
+
+                if ( userProfile.getUserOccupation().equals("AMBULANCE") )
+                    profileRegion.setText("Hospital      : " + userProfile.getUserRegion());
+                else
+                    profileRegion.setText("Region         : " + userProfile.getUserRegion());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ProfileActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ChangePwd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +84,17 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(new Intent(ProfileActivity.this , ChangePwdActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home :
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
